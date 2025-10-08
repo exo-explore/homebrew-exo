993c71490c9cc9af9449a00dbc3d4a80a5eb49df
Alex Cheema <alexcheema123@gmail.com>
2025-10-08 18:38:26 +0100

    bump version to 0.9.7

--- Formula/exo.rb
+++ Formula/exo.rb
@@ -9,7 +9,7 @@ class Exo < Formula
   url "https://github.com/exo-explore/exo.git",
     using: :git,
     branch: "super_secret_branch"
-  version "0.9.6"
+  version "0.9.7"
   sha256 ""
   license ""
 
